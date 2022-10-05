2f16a1cf6351f46ad3c1ddb7cc1eddea583b7438
Mawete Rafael <maweteana28@gmail.com>
2022-10-05 20:47:08 -0300

    adicionando-react

---
+++ my-app/src/componentes/main.js
@@ -0,0 +1,23 @@
+import React from "react"
+import "../assets/main.css";
+import "../App.css"
+import "../assets/main.css"
+
+
+
+function MyPrincipal(props) {
+    return (
+        <div className="main">
+            <div className="IMG">
+                <img  className="img"src={props.image}/>
+                <span>
+                   <h3> {props.name}</h3>
+                    <p> {props.EmailAdress}  </p>
+                     <p> {props.PhoneNumber} </p>
+                 </span>
+             </div>
+             
+        </div>
+    );
+}
+export default MyPrincipal;
