e78b480ef0d1dd0393b1cfa021f1c6a304026765
stwuar meneses <stwuarcarreno@gmail.com>
2022-10-06 00:49:52 -0300

    ediçao

--- my-app/src/Api.js
+++ my-app/src/Api.js
@@ -1,5 +1,3 @@
-
-
 import React from "react";
 import {Table, Button, Form, Modal} from "react-bootstrap";
 
--- my-app/src/navegacionApi.js
+++ my-app/src/navegacionApi.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import Api from './Api'
-import Home from './Home';
-import Sobre from './Sobre';
 import {BrowserRouter, Routes, Link, Route, } from 'react-router-dom';
 import {Nav} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -28,9 +26,7 @@ function Navegaçao() {
       
 
       <Routes>
-       <Route path="/" index element={<Home/>}></Route>
-       <Route path="/Api" element={<Api/>}></Route>
-       <Route path="/Sobre" element={<Sobre/>}></Route> 
+       <Route path="/Api" element={<Api/>}></Route> 
       </Routes>
 
       </BrowserRouter>
